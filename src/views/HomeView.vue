4097765214d2b0fe7ad347d83094ddd70d2d8b3d
yanasaputra18 <yanasaputra152@gmail.com>
2025-04-22 05:13:07 +0700

    push

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,470 @@
 <template>
+  <section>
+    <div class="container-fluid m-0">
+      <div class="row px-2">
+        <div class="col-12">
+          <div class="navbar">
+            <!-- <div class="logo">
+              <img src="../assets/logo.png" alt="" style="width: 4rem" />
+            </div> -->
+            <div class="search mt-1">
+              <input type="search" placeholder="Search.." />
+            </div>
+            <div class="profil">
+              <span><i class="fa-solid fa-user"></i></span>
+            </div>
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="icon d-flex justify-content-between mt-4">
+            <div class="main-menu">
+              <div class="menu p-2">
+                <span><i class="fa-regular fa-credit-card m-2"></i></span>
+              </div>
+              <p>Menu1</p>
+            </div>
+            <div class="main-menu">
+              <div class="menu p-2">
+                <span><i class="fa-solid fa-basket-shopping mt-2"></i></span>
+              </div>
+              <p>Menu1</p>
+            </div>
+            <div class="main-menu">
+              <div class="menu p-2">
+                <span><i class="fa-regular fa-font-awesome mt-2"></i></span>
+              </div>
+              <p>Menu1</p>
+            </div>
+          </div>
+          <div class="icon d-flex justify-content-between">
+            <div class="main-menu">
+              <div class="menu p-2">
+                <span><i class="fa-solid fa-basket-shopping mt-2"></i></span>
+              </div>
+              <p>Menu1</p>
+            </div>
+            <div class="main-menu">
+              <div class="menu p-2">
+                <span><i class="fa-regular fa-font-awesome mt-2"></i></span>
+              </div>
+              <p>Menu1</p>
+            </div>
+            <div class="main-menu">
+              <div class="menu p-2">
+                <span><i class="fa-solid fa-bars mt-2"></i></span>
+              </div>
+              <p>Menu1</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="SPONSOR">
+      <div class="row px-3 m-0">
+        <div class="col-12">
+          <div class="sponsor">
+            <h2>Sponsor</h2>
+            <div class="sponsor-main d-flex justify-content-between">
+              <div class="content"></div>
+              <div class="content1"></div>
+              <div class="content2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container-Promo mt-5 px-3">
+      <h3>Promo</h3>
+      <div class="row m-0 d-flex">
+        <div class="col-8 promo_utama"></div>
+        <div class="col-3">
+          <div class="promo_mini"></div>
+          <div class="promo_mini"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="row px-2 m-0">
+      <div class="col-12">
+        <div class="product-title my-4">
+          <h3>Product</h3>
+          <div
+            class="menu-title d-flex justify-content-between align-items-center"
+          >
+            <div class="cardt"></div>
+            <p>Category</p>
+            <p>Category</p>
+            <p>Category</p>
+          </div>
+          <div class="produk-main d-flex justify-content-between">
+            <div class="card" style="width: 7rem">
+              <img
+                src="../assets/product-thumb-12.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h6 class="card-title">Produk</h6>
+                <p class="card-text">1.000.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 7rem">
+              <img
+                src="../assets/product-thumb-12.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h6 class="card-title">Produk</h6>
+                <p class="card-text">1.000.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 7rem">
+              <img
+                src="../assets/product-thumb-12.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h6 class="card-title">Produk</h6>
+                <p class="card-text">1.000.</p>
+              </div>
+            </div>
+          </div>
+          <div class="produk-main mt-2 d-flex justify-content-between">
+            <div class="card" style="width: 7rem">
+              <img
+                src="../assets/product-thumb-12.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h6 class="card-title">Produk</h6>
+                <p class="card-text">1.000.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 7rem">
+              <img
+                src="../assets/product-thumb-12.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h6 class="card-title">Produk</h6>
+                <p class="card-text">1.000.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 7rem">
+              <img
+                src="../assets/product-thumb-12.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h6 class="card-title">Produk</h6>
+                <p class="card-text">1.000.</p>
+              </div>
+            </div>
+          </div>
+          <div class="produk-main mt-2 d-flex justify-content-between">
+            <div class="card" style="width: 7rem">
+              <img
+                src="../assets/product-thumb-12.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h6 class="card-title">Produk</h6>
+                <p class="card-text">1.000.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 7rem">
+              <img
+                src="../assets/product-thumb-12.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h6 class="card-title">Produk</h6>
+                <p class="card-text">1.000.</p>
+              </div>
+            </div>
+            <div class="card" style="width: 7rem">
+              <img
+                src="../assets/product-thumb-12.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h6 class="card-title">Produk</h6>
+                <p class="card-text">1.000.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="row px-2 m-0">
+      <div class="col-12">
+        <div class="maps my-3">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126912.62879063057!2d106.8226182025387!3d-6.2611416762204595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f2d148fbe713%3A0x6e667d52ebedf5a9!2sJakarta%20Timur%2C%20Kota%20Jakarta%20Timur%2C%20Daerah%20Khusus%20Ibukota%20Jakarta!5e0!3m2!1sid!2sid!4v1745269818275!5m2!1sid!2sid"
+            width="350"
+            height="150"
+            style="border: 0"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="row px-2 m-0">
+      <div class="col-12 d-flex justify-content-center">
+        <div class="foother">
+          <div class="logo">
+            <img src="../assets/logo.png" alt="" style="width: 15rem" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer class="bg-dark p-3 m-0 text-bg-danger">
+    <div class="sosial-media d-flex gap-4">
+      <p><i class="fa-brands fa-facebook"></i></p>
+      <p><i class="fa-brands fa-tiktok"></i></p>
+      <p><i class="fa-brands fa-instagram"></i></p>
+      <p><i class="fa-solid fa-x"></i></p>
+    </div>
+    <br />
+    <div class="menu_food d-flex justify-content-between">
+      <div class="footer-menu">
+        <h5 class="widget-title">Ultras</h5>
+        <ul class="menu-list list-unstyled">
+          <li class="menu-item">
+            <a href="#" class="nav-link">About us</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" class="nav-link">Conditions </a>
+          </li>
+          <li class="menu-item">
+            <a href="#" class="nav-link">Our Journals</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" class="nav-link">Careers</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" class="nav-link">Affiliate Programme</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" class="nav-link">Ultras Press</a>
+          </li>
+        </ul>
+      </div>
+      <div class="footer-menu">
+        <h5 class="widget-title">Customer Service</h5>
+        <ul class="menu-list list-unstyled">
+          <li class="menu-item">
+            <a href="#" class="nav-link">FAQ</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" class="nav-link">Contact</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" class="nav-link">Privacy Policy</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" class="nav-link">Returns & Refunds</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" class="nav-link">Cookie Guidelines</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" class="nav-link">Delivery Information</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <br />
+    <p class="text-center">© 2025 Foodmart</p>
+  </footer>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+<style scoped>
+/* Navbar Stard */
+.container-fluid {
+  background-color: rgb(6, 111, 241);
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+}
+.navbar {
+  width: 100% !important;
+  height: 5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.search {
+  width: 18rem;
+  height: 2.5rem;
+  border-radius: 20px;
+  background-color: white;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.search input {
+  width: 100% !important;
+  height: 3rem;
+  padding: 0 1rem;
+  border: none;
+  outline: none;
+}
+.profil {
+  width: 3rem;
+  height: 3rem;
+  background-color: white;
+  padding: 1rem;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.profil span i {
+  font-size: 1rem;
+}
+/* Navbar End */
+
+/* icon strat */
+
+.main-menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.main-menu p {
+  color: #ffff;
+}
+.menu {
+  text-align: center;
+  width: 5rem;
+  height: 4rem;
+  background-color: antiquewhite;
+  border-radius: 5px;
+}
+.menu span i {
+  font-size: 2rem;
+}
+
+/* icon end */
+
+/* sponsor stard */
+.sponsor {
+  margin-top: 2rem;
+}
+.content {
+  margin-top: 1rem;
+  width: 6.5rem;
+  height: 3rem;
+  border-radius: 0.5rem;
+  background-image: url(../assets/tessss.png);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+.content1 {
+  margin-top: 1rem;
+  width: 6.5rem;
+  height: 3rem;
+  border-radius: 0.5rem;
+  background-image: url(../assets/make.png);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+.content2 {
+  margin-top: 1rem;
+  width: 6.5rem;
+  height: 3rem;
+  border-radius: 0.5rem;
+  background-image: url(../assets/pep.jpg);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 9rem;
+}
+/* sponsor end */
+
+/* promo star */
+.container-Promo {
+  background-color: rgb(6, 111, 241);
+  color: #ffff;
+  padding: 1.5rem 0;
+}
+.promo_utama {
+  height: 8rem;
+  background-image: url(../assets/pro.jpg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 5px;
+}
+.promo_mini {
+  width: 6.4rem !important;
+  height: 3.8rem;
+  margin-bottom: 0.5rem;
+  background-image: url(../assets/product-thumb-11.jpg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 4px;
+}
+/* promo end */
+
+/* product str */
+.cardt {
+  margin-top: -1rem !important;
+  width: 5rem;
+  height: 1rem;
+  border-radius: 1rem;
+  background-color: rgb(177, 177, 180);
+}
+/* product end */
+
+/* foother star */
+.sosial-media {
+  color: #ffff;
+  font-size: 1.5rem;
+}
+/* foother end */
+</style>
